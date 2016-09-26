03461b8aa9525c654989dac0e79081f56d9dba61
b-gran <bill@billgranfield.com>
2016-09-26 15:40:33 -0400

    Beefs up test files

--- src/test/index.js
+++ src/test/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import * as Schema from '../Schema';
-import Editor from '../Editor';
+import { ObjectEditor, ArrayEditor } from '../Editor';
 import { PropTypes } from '../constants';
 
 import update from 'react-addons-update';
@@ -24,12 +24,28 @@ const schema = {
     },
 };
 
+function updateArray (updated, idx) {
+    console.log('args', arguments);
+    this.setState({
+        object: update(
+            this.state.object,
+            {
+                [idx]: {
+                    $set: updated
+                }
+            }
+        )
+    })
+};
+
 class Wrapper extends React.Component {
     static displayName = 'Wrapper';
 
     static propTypes = {
         initialObject: React.PropTypes.any.isRequired,
         type: PropTypes.Schema.isRequired,
+
+        onUpdate: React.PropTypes.func,
     };
 
     constructor (props) {
@@ -39,33 +55,31 @@ class Wrapper extends React.Component {
             object: props.initialObject
         };
 
-        this.change = this.change.bind(this);
+        this.change = (this.props.onUpdate && this.props.onUpdate.bind(this)) ||
+                updateArray.bind(this);
     }
 
-    change = function (updated, idx) {
-        console.log('args', arguments);
-        this.setState({
-            object: update(
-                this.state.object,
-                {
-                    [idx]: {
-                        $set: updated
-                    }
-                }
-            )
-        })
+
+    add = newObject => {
+        console.log('adding', newObject);
+        this.setState({ object: [ ...this.state.object, newObject ]});
+        return true;
     };
 
     render () {
+        const EditorComponent = Array.isArray(this.state.object)
+            ? ArrayEditor
+            : ObjectEditor;
+
         return (
         <div>
-            <Editor
+            <EditorComponent
                 className='editor--outside'
                 object={this.state.object}
                 type={this.props.type}
                 onUpdateElement={this.change}
-                onRemoveElement={empty}
-                onAddElement={empty} />;
+                onAddElement={this.add}
+                onRemoveElement={empty} />
 
             <p>
                 { JSON.stringify(this.state.object, null, '\t')}
@@ -103,4 +117,26 @@ const b = <Wrapper
 render(
     b,
     document.getElementById('app')
+);
+
+const c = <Wrapper
+    initialObject={{
+        foo: 'something',
+        bar: 4,
+        baz: {
+            biz: 3,
+            booz: {
+                barz: {
+                    nested: 2
+                }
+            }
+        }
+    }}
+    type={schema}
+    onUpdate={function (updated) { this.setState({ object: updated }) }}
+/>;
+
+render(
+    c,
+    document.getElementById('app2')
 );
