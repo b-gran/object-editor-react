1f7764d922dde7a9d6c3db81282f20fbbfba27b9
b-gran <bill@billgranfield.com>
2016-09-28 17:44:04 -0400

    Adds npm package entry point file (just some exports)

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*
+ * Entry point for the npm package.
+ *
+ * This file exposes the package's API and exports.
+ */
+
+// SchemaTypes
+export { SchemaTypes, matchesSchema } from './Schema';
+
+// ObjectEditor, ArrayEditor
+export { ObjectEditor, ArrayEditor } from './Editor';
+
+// PropTypes
+export { PropTypes } from './constants';
