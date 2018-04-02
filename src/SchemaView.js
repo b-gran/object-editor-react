d3b2f9e87efd5bb1d5ed2ad4d2ee21a8fbdf8cf7
b-gran <bill@billgranfield.com>
2018-04-02 10:21:39 +0100

    Fully working schema view

--- src/SchemaView.js
+++ src/SchemaView.js
@@ -6,7 +6,7 @@ import React from 'react'
 import { PropTypes as Props } from './constants'
 import PropTypes from 'prop-types'
 
-import glamorous, { Div } from 'glamorous'
+import { Div, Span } from 'glamorous'
 
 import * as _ from 'lodash'
 
@@ -48,6 +48,12 @@ export default class SchemaView extends React.Component {
     expanded: false,
   }
 
+  toggleExpanded () {
+    this.setState({
+      expanded: !this.state.expanded,
+    })
+  }
+
   render () {
     const identifier = getSchemaTypeIdentifier(this.props.schema)
     switch (identifier) {
@@ -60,37 +66,34 @@ export default class SchemaView extends React.Component {
       case SCHEMA_TYPE_IDENTIFIER.array:
       case SCHEMA_TYPE_IDENTIFIER.object:
         if (typeof this.props.keyName === 'string') {
-          return <Div display="flex" flexDirection="row">
-            <Div marginRight="10px" css={keyName}>{this.props.keyName}:</Div>
-            <div><SchemaLeaf schemaTypeName={identifier}/></div>
-          </Div>
+          return <KeyValueSchemaView
+            preview={<LeafSchema schemaTypeName={identifier} />}
+            schemaElement={<LeafSchema schemaTypeName={identifier} />}
+            keyName={this.props.keyName}/>
         }
-        return <SchemaLeaf schemaTypeName={identifier}/>
+
+        return <LeafSchema schemaTypeName={identifier} />
 
       case SCHEMA_TYPE_IDENTIFIER.shape:
         if (typeof this.props.keyName === 'string') {
-          return <Div display="flex" flexDirection="column">
-            <Div display="flex" flexDirection="row">
-              <Div marginRight="10px" css={keyName}>{this.props.keyName}:</Div>
-              <Div css={monospace}>Shape</Div>
-            </Div>
-
-            <Div marginLeft="20px"><ShapeSchema schema={this.props.schema}/></Div>
-          </Div>
+          return <KeyValueSchemaView
+            schemaElement={<ShapeSchema schema={this.props.schema}/>}
+            keyName={this.props.keyName}
+            onToggleExpanded={this.toggleExpanded.bind(this)}
+            preview="Shape"
+            expanded={this.state.expanded}/>
         }
 
         return <ShapeSchema schema={this.props.schema}/>
 
       case SCHEMA_TYPE_IDENTIFIER.arrayOf:
         if (typeof this.props.keyName === 'string') {
-          return <Div display="flex" flexDirection="column">
-            <Div display="flex" flexDirection="row">
-              <Div marginRight="10px" css={keyName}>{this.props.keyName}:</Div>
-              <Div css={monospace}>ArrayOf</Div>
-            </Div>
-
-            <Div marginLeft="20px"><SchemaView schema={this.props.schema._elementType}/></Div>
-          </Div>
+          return <KeyValueSchemaView
+            schemaElement={<SchemaView schema={this.props.schema._elementType}/>}
+            keyName={this.props.keyName}
+            onToggleExpanded={this.toggleExpanded.bind(this)}
+            preview="ArrayOf"
+            expanded={this.state.expanded}/>
         }
 
         return <Div display="flex" flexDirection="column">
@@ -101,6 +104,17 @@ export default class SchemaView extends React.Component {
       default:
         throw new Error('invalid schema type identifier')
     }
+  }
+}
+
+class LeafSchema extends React.Component {
+  static displayName = 'LeafSchema'
+  static propTypes = {
+    schemaTypeName: PropTypes.node.isRequired,
+  }
+
+  render () {
+    return <Div css={monospace}>{this.props.schemaTypeName}</Div>
   }
 }
 
@@ -121,13 +135,63 @@ class ShapeSchema extends React.Component {
   }
 }
 
-class SchemaLeaf extends React.Component {
-  static displayName = 'SchemaLeaf'
+class KeyValueSchemaView extends React.Component {
+  static displayName = 'KeyValueSchemaView'
   static propTypes = {
-    schemaTypeName: PropTypes.node.isRequired,
+    // Rendering of the actual schema
+    schemaElement: PropTypes.node.isRequired,
+
+    // For expandable schemas, the preview of the schema to show in line with the key
+    preview: PropTypes.node.isRequired,
+
+    keyName: PropTypes.string.isRequired,
+    expanded: PropTypes.bool,
+    onToggleExpanded: PropTypes.func,
   }
 
   render () {
-    return <Div css={monospace}>{ this.props.schemaTypeName }</Div>
+    // Non-expandable key/value pairs
+    if (!this.props.onToggleExpanded) {
+      return (
+        <Div display="flex" cursor="default">
+          <Div width="16px"/>
+          <Div display="flex" flexDirection="row">
+            <Div marginRight="10px" css={keyName}
+                 onClick={this.props.onToggleExpanded}>{this.props.keyName}:</Div>
+            <Div css={monospace}>{this.props.preview}</Div>
+          </Div>
+        </Div>
+      )
+    }
+
+    if (!this.props.expanded) {
+      return (
+        <Div display="flex" cursor="default">
+          <Div display="flex" width="16px" onClick={this.props.onToggleExpanded}>
+            <Span fontSize="9px" padding="2px">▶</Span>
+          </Div>
+          <Div display="flex" flexDirection="row" onClick={this.props.onToggleExpanded}>
+            <Div marginRight="10px" css={keyName}>{this.props.keyName}:</Div>
+            <Div css={monospace}>{this.props.preview}</Div>
+          </Div>
+        </Div>
+      )
+    }
+
+    return (
+      <Div display="flex" cursor="default">
+        <Div display="flex" width="16px" onClick={this.props.onToggleExpanded}>
+          <Span fontSize="9px" padding="2px">▼</Span>
+        </Div>
+        <Div display="flex" flexDirection="column">
+          <Div display="flex" flexDirection="row" onClick={this.props.onToggleExpanded}>
+            <Div marginRight="10px" css={keyName}>{this.props.keyName}:</Div>
+            <Div css={monospace} >{this.props.preview}</Div>
+          </Div>
+
+          <Div>{ this.props.schemaElement }</Div>
+        </Div>
+      </Div>
+    )
   }
 }
