830bbbdf0ab08fd478ead1f3880818d778ad7d67
b-gran <bill@billgranfield.com>
2018-04-08 09:58:24 +0100

    Add separate commands for building library & examples

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -3,6 +3,7 @@ const clean = require('@neutrinojs/clean');
 const minify = require('@neutrinojs/minify');
 const { optimize } = require('webpack');
 const nodeExternals = require('webpack-node-externals');
+const R = require('ramda')
 
 const EXAMPLES_DIR = path.resolve(path.join(__dirname, 'examples'))
 const SRC_DIR = path.resolve(path.join(__dirname, 'src'))
@@ -22,8 +23,6 @@ module.exports = {
       libraryEntries: {
         index: path.join(SRC_DIR, 'index.js'),
       },
-
-      clean: true,
     },
   },
   use: [
@@ -42,36 +41,47 @@ module.exports = {
 
     // Set up builds of the library
     neutrino => {
-      const options = neutrino.options.library
-      neutrino.config.when(neutrino.options.command === 'build:library', () => {
-        // We build as a library, so don't use any webpack chunking.
-        // If these plugins are used, the library module won't be importable
-        // because the dependencies in the chunk will be missing.
-        neutrino.config.when(
-          neutrino.config.plugins.has('runtime-chunk'),
-          (config) => {
-            config.plugins
-              .delete('runtime-chunk')
-              .delete('vendor-chunk')
-              .delete('named-modules')
-              .delete('named-chunks')
-              .delete('name-all');
-          }
-        )
+      const options = R.mergeDeepLeft({
+        clean: neutrino.options.library.clean !== false && {
+          paths: [neutrino.options.output]
+        },
+      })(neutrino.options.library)
 
-        neutrino.config.when(options.clean, () => neutrino.use(clean, options.clean));
+      neutrino.config.when(
+        neutrino.options.command === 'build' &&
+        neutrino.options.args._.includes('library'),
+        () => {
+          // We build as a library, so don't use any webpack chunking.
+          // If these plugins are used, the library module won't be importable
+          // because the dependencies in the chunk will be missing.
+          neutrino.config.when(
+            neutrino.config.plugins.has('runtime-chunk'),
+            (config) => {
+              config.plugins
+                .delete('runtime-chunk')
+                .delete('vendor-chunk')
+                .delete('named-modules')
+                .delete('named-chunks')
+                .delete('name-all');
+            }
+          )
 
-        // Add the files in "libraryEntries" as webpack entry points.
-        Object
-          .keys(options.libraryEntries)
-          .forEach(key => neutrino.config.entry(key).add(options.libraryEntries[key]))
+          neutrino.config.when(
+            options.clean,
+            () => neutrino.use(clean, options.clean)
+          );
 
-        neutrino.config
+          // Add the files in "libraryEntries" as webpack entry points.
+          Object
+            .keys(options.libraryEntries)
+            .forEach(key => neutrino.config.entry(key).add(options.libraryEntries[key]))
+
           // Set up the actual library build
-          .devtool('source-map')
-          .target('web')
-          .context(neutrino.options.root)
-          .output
+          neutrino.config
+            .devtool('source-map')
+            .target('web')
+            .context(neutrino.options.root)
+            .output
             .path(neutrino.options.output)
             .library(options.name)
             .filename('[name].js')
@@ -79,26 +89,16 @@ module.exports = {
             .umdNamedDefine(true)
             .end()
 
-          // Print webpack stats
-          .when(neutrino.options.debug, (config) => {
-            config.merge({
-              stats: {
-                maxModules: Infinity,
-                optimizationBailout: true
-              }
-            });
-          })
 
-          // Reduce file size by separating externals & minifying
-          .when(process.env.NODE_ENV !== 'test', config => config.externals([nodeExternals()]))
-          .when(process.env.NODE_ENV === 'production', (config) => {
-            neutrino.use(minify);
-            config
-              .plugin('module-concat')
+            // Reduce file size by separating externals & minifying
+            .when(process.env.NODE_ENV !== 'test', config => config.externals([nodeExternals()]))
+            .when(process.env.NODE_ENV === 'production', (config) => {
+              neutrino.use(minify);
+              config
+                .plugin('module-concat')
                 .use(optimize.ModuleConcatenationPlugin);
-          })
-
-      })
+            })
+        })
     },
   ]
 };
